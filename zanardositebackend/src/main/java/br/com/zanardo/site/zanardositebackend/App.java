755fd93eb73e7b213af425a169c4d22efbdd0993
Julio <zanardo.it@gmail.com>
2019-06-04 22:58:10 -0300

    Commit de configuracao

---
+++ zanardositebackend/src/main/java/br/com/zanardo/site/zanardositebackend/App.java
@@ -0,0 +1,13 @@
+package br.com.zanardo.site.zanardositebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
